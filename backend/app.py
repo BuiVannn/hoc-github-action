dc9c9895f5c43430a486ce2afcc3d88f8de8cd4a
BuiVannn <buivanphuctho@gmail.com>
2026-02-19 12:43:52 +0700

    update app

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,7 @@ def phep_cong(a, b):
 ket_qua = phep_cong(5, 10)
 if ket_qua == 15:
     print("✅ Test thành công: 5 + 10 = 15")
+    print("say oh yeah")
 else:
     print("❌ Test thất bại!")
     exit(1) # Lệnh này báo cho GitHub Action biết là job bị lỗi
